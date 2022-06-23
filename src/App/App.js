3a1105772842581ca7f9fec11b5d4d5520e66350
Dheeraj Kushwah <dheeraj.kushwah@coditation.com>
2022-06-23 11:47:18 +0530

    Added packages.

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from '../components';
+import './App.scss';
+import 'antd/dist/antd.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import { NavigationItems } from './components';
+
+function Navbar() {
+  return (
+    <div>
+      <NavigationItems />
+    </div>
+  );
+}
+
+export default Navbar;
